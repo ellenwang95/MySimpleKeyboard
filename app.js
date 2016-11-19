88a43a14f69003879f43b1993b08131ca9617d8b
Ellen Wang <ellenwang95@yahoo.ca>
2016-11-19 12:45:58 +0100

    basic structure

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+var levelup = require('levelup');
+var routes = require('./router/routes');
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+// /css, /js 
+app.use(express.static(__dirname + '/public'));
+
+//routing 
+app.set('view engine', 'pug');
+app.use('/', routes);
+
+server.listen(3000);
+console.log("Running at Port 3000");
+
+module.exports = app;
